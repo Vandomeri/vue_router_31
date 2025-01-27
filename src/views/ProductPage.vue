500a1fa088df563468d71ad267e1977fd2f4faa9
Vando <vandomeri@gmail.com>
2025-01-27 09:56:42 +0300

    first commit

---
+++ src/views/ProductPage.vue
@@ -0,0 +1,33 @@
+<template>
+  <div v-if="product">
+    <h1>Товар с id: {{ $route.params.id }}</h1>
+    <img v-for="image in product.images" :key="image" :src="image" alt="">
+    <h2>{{ product.title }}</h2>
+    <p>{{ product.price }}</p>
+  </div>
+</template>
+
+<style scoped>
+img {
+  width: 50%;
+}
+</style>
+
+<script setup>
+import { onMounted, ref } from 'vue';
+import { useRoute } from 'vue-router';
+
+
+const route = useRoute()
+
+
+
+const product = ref(null)
+
+onMounted(() => {
+  fetch(`https://dummyjson.com/products/${route.params.id}`)
+    .then((resp) => resp.json())
+    .then((json) => product.value = json)
+})
+
+</script>
---
+++ src/views/ProductsPage.vue
@@ -0,0 +1,40 @@
+<template>
+
+  <div class="grid">
+    <RouterLink :to="`/product/${product.id}`" v-for="product in products" :key="product.id">
+      <img :src="product.images[0]" alt="">
+      <p>{{ product.title }}</p>
+    </RouterLink>
+  </div>
+
+</template>
+
+
+<style scoped>
+.grid {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  gap: 30px;
+}
+
+.grid img {
+  width: 100%;
+}
+</style>
+
+<script setup>
+import { onMounted, ref } from 'vue';
+import { RouterLink } from 'vue-router';
+
+
+const products = ref([])
+
+
+onMounted(() => {
+  fetch('https://dummyjson.com/products')
+    .then((resp) => resp.json())
+    .then((json) => products.value = json.products)
+})
+
+
+</script>
